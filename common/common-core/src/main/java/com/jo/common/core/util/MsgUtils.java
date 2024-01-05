03a8fd1d3b82525356adac8e5c120171bcb16d0a
Jo <syunn90@163.com>
2024-01-05 15:32:55 +0800

    异常信息配置

--- common/common-core/src/main/java/com/jo/common/core/util/MsgUtils.java
+++ common/common-core/src/main/java/com/jo/common/core/util/MsgUtils.java
@@ -13,34 +13,34 @@ import java.util.Locale;
 @UtilityClass
 public class MsgUtils {
 
-//	/**
-//	 * 通过code 获取中文错误信息
-//	 * @param code
-//	 * @return
-//	 */
-//	public String getMessage(String code) {
-//		MessageSource messageSource = SpringContextHolder.getBean("messageSource");
-//		return messageSource.getMessage(code, null, Locale.CHINA);
-//	}
-//
-//	/**
-//	 * 通过code 和参数获取中文错误信息
-//	 * @param code
-//	 * @return
-//	 */
-//	public String getMessage(String code, Object... objects) {
-//		MessageSource messageSource = SpringContextHolder.getBean("messageSource");
-//		return messageSource.getMessage(code, objects, Locale.CHINA);
-//	}
-//
-//	/**
-//	 * security 通过code 和参数获取中文错误信息
-//	 * @param code
-//	 * @return
-//	 */
-//	public String getSecurityMessage(String code, Object... objects) {
-//		MessageSource messageSource = SpringContextHolder.getBean("securityMessageSource");
-//		return messageSource.getMessage(code, objects, Locale.CHINA);
-//	}
+	/**
+	 * 通过code 获取中文错误信息
+	 * @param code
+	 * @return
+	 */
+	public String getMessage(String code) {
+		MessageSource messageSource = SpringContextHolder.getBean("messageSource");
+		return messageSource.getMessage(code, null, Locale.CHINA);
+	}
+
+	/**
+	 * 通过code 和参数获取中文错误信息
+	 * @param code
+	 * @return
+	 */
+	public String getMessage(String code, Object... objects) {
+		MessageSource messageSource = SpringContextHolder.getBean("messageSource");
+		return messageSource.getMessage(code, objects, Locale.CHINA);
+	}
+
+	/**
+	 * security 通过code 和参数获取中文错误信息
+	 * @param code
+	 * @return
+	 */
+	public String getSecurityMessage(String code, Object... objects) {
+		MessageSource messageSource = SpringContextHolder.getBean("securityMessageSource");
+		return messageSource.getMessage(code, objects, Locale.CHINA);
+	}
 
 }
