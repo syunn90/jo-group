7d7acc27783c8cbdec5a9c5d3846b83470b33288
Jo <syunn90@163.com>
2024-04-05 15:19:45 +0800

    sse

--- jo-ujms/biz/src/main/java/com/jo/biz/demo/DemoFluxController.java
+++ jo-ujms/biz/src/main/java/com/jo/biz/demo/DemoFluxController.java
@@ -1,32 +1,25 @@
-//package com.jo.biz.demo;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.http.codec.ServerSentEvent;
-//import org.springframework.integration.dsl.IntegrationFlow;
-//import org.springframework.integration.dsl.MessageChannels;
-//import org.springframework.messaging.MessageHandler;
-//import org.springframework.messaging.SubscribableChannel;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import reactor.core.publisher.Flux;
-//import reactor.core.publisher.FluxSink;
-//import reactor.util.function.Tuples;
-//
-//import java.time.Duration;
-//import java.util.concurrent.ThreadLocalRandom;
-//
-///**
-// * 异步响应接口
-// * @author xtc
-// * @date 2024/2/5
-// */
-//@RestController
-//@RequestMapping("/demo/sse")
-//@Slf4j
-//public class DemoFluxController {
+package com.jo.biz.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 异步响应接口
+ * @author xtc
+ * @date 2024/2/5
+ */
+@RestController
+@RequestMapping("/sse/c")
+@Slf4j
+public class DemoFluxController {
 //
 //    private String s;
 //
@@ -70,4 +63,39 @@
 //        });
 //
 //    }
-//}
+private static Map<String, SseEmitter> sseCache = new ConcurrentHashMap<>();
+    @GetMapping(path = "subscribe", produces = {MediaType.TEXT_EVENT_STREAM_VALUE})
+    public SseEmitter push(String id) throws IOException {
+        // 超时时间设置为3s，用于演示客户端自动重连
+        SseEmitter sseEmitter = new SseEmitter();
+        // 设置前端的重试时间为1s
+        sseEmitter.send(SseEmitter.event().reconnectTime(1000).data("连接成功"));
+        sseCache.put(id, sseEmitter);
+        System.out.println("add " + id);
+        sseEmitter.onTimeout(() -> {
+            System.out.println(id + "超时");
+            sseCache.remove(id);
+        });
+        sseEmitter.onCompletion(() -> System.out.println("完成！！！"));
+        return sseEmitter;
+    }
+
+    @GetMapping(path = "push")
+    public String push(String id, String content) throws IOException {
+        SseEmitter sseEmitter = sseCache.get(id);
+        if (sseEmitter != null) {
+            sseEmitter.send(SseEmitter.event().name("msg").data("message from service：" + content));
+        }
+        return "over";
+    }
+
+    @GetMapping(path = "over")
+    public String over(String id) {
+        SseEmitter sseEmitter = sseCache.get(id);
+        if (sseEmitter != null) {
+            sseEmitter.complete();
+            sseCache.remove(id);
+        }
+        return "over";
+    }
+}
